9bfa63631d08e95cfbb2d317fc8e0184afcb06ae
Stefani Arabadjieva <s.arabadjieva00@gmail.com>
2021-07-05 21:56:09 +0300

    Small fixes, removed unused plugins

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private bool hasReachedEnd = false;
+
+    public Vector3 movePositionWith;
+    public float speed = 1f;
+
+    void FixedUpdate()
+    {
+        /* if (GameManager.Instance.paused) return;
+
+         if (!hasReachedEnd && (transform.localPosition.x == 150 || transform.localPosition.x <= 0))
+         {
+             movePositionWith = -movePositionWith;
+             transform.rotation =  Quaternion.Euler(0, transform.rotation.y + 180f, 0f);
+             hasReachedEnd = true;
+         }
+
+         transform.position += movePositionWith * Time.fixedDeltaTime * speed;
+     }*/
+    }
+}
